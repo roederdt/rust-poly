af8f325cef3d8795e706f8c29b113dab00816278
roederdt <123527836+roederdt@users.noreply.github.com>
2024-09-25 15:39:34 -0700

    Created Sub method as a copy of add with different operators inside

--- poly/src/lib.rs
+++ poly/src/lib.rs
@@ -30,6 +30,33 @@ impl std::ops::Add for Poly {
     }
 }
 
+impl std::ops::Sub for Poly {
+    type Output = Self;
+
+    pub fn sub(&self, poly2: &self) -> Self {
+        if self.power < poly2.power {
+            return poly2 - self;
+        }
+        let temp_arr = vec![0; self.power];
+        temp_arr = temp_arr
+            .iter()
+            .enumerate()
+            .map(|p| {
+                if p > poly2.power {
+                    self.values[self.power - p]
+                } else {
+                    self.values[self.power - p] - poly2.values[poly2.power - p]
+                }
+            })
+            .collect::<Vec<_>>();
+        Poly {
+            power: self.power,
+            values: temp_arr,
+            rem: None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
