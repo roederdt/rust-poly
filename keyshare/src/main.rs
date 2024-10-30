f54c84575e3248ed749b6377ec9194c27188a7c2
roederdt <123527836+roederdt@users.noreply.github.com>
2024-10-29 22:20:51 -0700

    nn secret shared the key and set up reading the args

--- keyshare/src/main.rs
+++ keyshare/src/main.rs
@@ -3,23 +3,42 @@ use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
     ChaCha20Poly1305, Nonce,
 };
+use nn_secret_share;
+use std::fs;
+use std::{env, num::ParseIntError};
 
-pub struct Error{
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Parsing int error {0}")]
+    ParsingError(#[from] ParseIntError),
+    #[error("Invalid argument")]
+    InvalidArgError,
     #[error("ChaCha error: {0}")]
-    ChaChaError(#[from] ChaCha20Poly1305::Error),
+    ChaChaError(#[from] chacha20poly1305::Error),
     #[error("Base64 error: {0}")]
-    Base64Error(#[from] base64::Error),
+    Base64Error(#[from] base64::DecodeError),
 }
-fn main() -> Result<(), base64::DecodeError> {
+fn main() -> Result<(), Error> {
     assert_eq!(BASE64_STANDARD.decode(b"+uwgVQA=")?, b"\xFA\xEC\x20\x55\0");
     assert_eq!(BASE64_STANDARD.encode(b"\xFF\xEC\x20\x55\0"), "/+wgVQA=");
 
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 4 {
+        return Err(Error::InvalidArgError);
+    }
+    let file_path = &args[1];
+    let num_shares = &args[2];
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
     let cipher = ChaCha20Poly1305::new(&key);
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref())?;
+
+    let ciphertext = cipher.encrypt(&nonce, contents.as_bytes())?;
     let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref())?;
-    assert_eq!(&plaintext, b"plaintext message");
+
+    let enc_keys = nn_secret_share::encode(&key, num_shares.parse()?);
 
     Ok(())
 }
