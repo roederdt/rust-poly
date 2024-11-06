2dfab02248a2ecd4fac6bf39a276f696316805a1
roederdt <123527836+roederdt@users.noreply.github.com>
2024-11-05 20:12:26 -0800

    added errors for io::error and serde_json::error as well as removed clone statements

--- keyshare/src/main.rs
+++ keyshare/src/main.rs
@@ -8,20 +8,24 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::{env, num::ParseIntError};
 
-#[derive(Clone, Debug, thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Parsing int error {0}")]
     ParsingError(#[from] ParseIntError),
-    #[error("Invalid argument")]
-    InvalidArgError,
+    #[error("Invalid argument:{0}")]
+    InvalidArgError(String),
     #[error("ChaCha error: {0}")]
     ChaChaError(#[from] chacha20poly1305::Error),
     #[error("Base64 error: {0}")]
     Base64Error(#[from] base64::DecodeError),
     #[error("NNShare decode error: {0}")]
-    NNError(#[from] nn_secret_share::Error),
+    NnError(#[from] nn_secret_share::Error),
     #[error("UTF8 decode error: {0}")]
-    UTF8Error(#[from] std::string::FromUtf8Error),
+    Utf8Error(#[from] std::string::FromUtf8Error),
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("serde_json error: {0}")]
+    SerdeJsonError(#[from] serde_json::Error),
 }
 #[derive(Serialize, Deserialize)]
 struct CipherIv {
@@ -30,7 +34,7 @@ struct CipherIv {
 }
 
 fn encode(infile: &String, num_shares: usize) -> Result<(String, String, Vec<String>), Error> {
-    let in_contents = fs::read_to_string(infile).expect(&format!("{infile}"));
+    let in_contents = fs::read_to_string(infile)?;
 
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
     let cipher = ChaCha20Poly1305::new(&key);
@@ -43,7 +47,7 @@ fn encode(infile: &String, num_shares: usize) -> Result<(String, String, Vec<Str
 
     let mut b64_keys_list = Vec::new();
     for i in 0..num_shares {
-        b64_keys_list.push(BASE64_STANDARD.encode(enc_keys[i].clone()));
+        b64_keys_list.push(BASE64_STANDARD.encode(&enc_keys[i]));
     }
 
     Ok((nonce, ciphertext, b64_keys_list))
@@ -56,7 +60,7 @@ fn decode(
 ) -> Result<String, Error> {
     let mut keys_list = Vec::new();
     for i in 0..b64_keys_list.len() {
-        keys_list.push(BASE64_STANDARD.decode(b64_keys_list[i].clone())?);
+        keys_list.push(BASE64_STANDARD.decode(&b64_keys_list[i])?);
     }
 
     let nonce = BASE64_STANDARD.decode(nonce)?;
@@ -73,12 +77,17 @@ fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        return Err(Error::InvalidArgError);
+        return Err(Error::InvalidArgError(format!(
+            "Must provide a subcommand argument"
+        )));
     }
     let kind = &args[1];
     if kind == "encode" {
         if args.len() != 5 {
-            return Err(Error::InvalidArgError);
+            return Err(Error::InvalidArgError(format!(
+                "Must provide exactly 4 arguments. Provided {}",
+                args.len() - 1
+            )));
         }
         let infile = &args[2];
         let num_shares: usize = args[3].parse()?;
@@ -86,36 +95,37 @@ fn main() -> Result<(), Error> {
 
         let (nonce, ciphertext, keys_vec) = encode(infile, num_shares)?;
 
-        if !(fs::exists(format!("./target/debug/{key_name}")).unwrap()) {
-            fs::create_dir(format!("./target/debug/{key_name}")).unwrap();
+        if !(fs::exists(format!("./target/debug/{key_name}"))?) {
+            fs::create_dir(format!("./target/debug/{key_name}"))?;
         }
 
         let iv_cipher = CipherIv { nonce, ciphertext };
         fs::write(
             format!("./target/debug/{key_name}/cipher_iv"),
-            serde_json::to_string(&iv_cipher).unwrap(),
-        )
-        .expect("Unable to write file");
+            serde_json::to_string(&iv_cipher)?,
+        )?;
 
         for i in 0..keys_vec.len() {
             fs::write(
                 format!("./target/debug/{key_name}/{key_name}{i}"),
-                keys_vec[i].clone(),
-            )
-            .expect("Unable to write file");
+                &keys_vec[i],
+            )?;
         }
     } else {
         if kind == "decode" {
             if args.len() != 4 {
-                return Err(Error::InvalidArgError);
+                return Err(Error::InvalidArgError(format!(
+                    "Must provide exactly 3 arguments. Provided {}",
+                    args.len() - 1
+                )));
             }
             let file_name = &args[2];
             let num_files = args[3].parse()?;
 
             let cipher_iv_string =
-                fs::read_to_string(format!("./target/debug/{file_name}/cipher_iv")).unwrap();
+                fs::read_to_string(format!("./target/debug/{file_name}/cipher_iv"))?;
 
-            let cipher_iv: CipherIv = serde_json::from_str(&cipher_iv_string).unwrap();
+            let cipher_iv: CipherIv = serde_json::from_str(&cipher_iv_string)?;
 
             let nonce = cipher_iv.nonce;
             let ciphertext = cipher_iv.ciphertext;
@@ -124,16 +134,16 @@ fn main() -> Result<(), Error> {
             let mut temp_file;
             for i in 0..num_files {
                 temp_file =
-                    fs::read_to_string(format!("./target/debug/{file_name}/{file_name}{i}"))
-                        .unwrap();
+                    fs::read_to_string(format!("./target/debug/{file_name}/{file_name}{i}"))?;
                 keys_vec.push(temp_file);
             }
 
             let plaintext = decode(&nonce, &ciphertext, &keys_vec)?;
-            fs::write("./target/debug/plaintext_testAgain", plaintext)
-                .expect("Unable to write file");
+            fs::write("./target/debug/plaintext_testAgain", plaintext)?;
         } else {
-            return Err(Error::InvalidArgError);
+            return Err(Error::InvalidArgError(format!(
+                "The subcommand argument must be encode/decode"
+            )));
         }
     }
     Ok(())
